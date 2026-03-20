d1a27c33d7d562e7a9bca67c40c757083e7e94e1
JonathanRosas14 <tijonathanrosas01@gmail.com>
2026-03-20 15:27:00 -0500

    subir listas

---
+++ listas.js
@@ -0,0 +1,17 @@
+let lista = [1, 2, 3, 4, 5];
+//agregar elemento al final de la lista
+lista.push(6);
+//agregar elemento al principio de la lista
+lista.unshift(0);
+//eliminar ultimo elemnento de lista
+lista.pop();
+//eliminar primer elemento de lista
+lista.shift();
+//imprimir la lista
+console.log(lista);
+
+
+let nombres = ["Juan", "Gaby", "Carlos", "Vivi"];
+for (let i = 0; i < nombres.length; i++){
+    console.log (nombres[i]);
+}
